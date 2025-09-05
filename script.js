68913bc0145a52cf2f75d0ed56bc9a35fb75b3a9
shraddhaPJ <shanejasa523@gmail.com>
2025-09-05 21:49:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // ===============================
 // Mobile Menu Toggle
 // ===============================
@@ -19,25 +19,3 @@ function openModal() {
 function closeModal() {
   document.getElementById("enquiryModal").classList.add("hidden");
 }
-=======
-// ===============================
-// Mobile Menu Toggle
-// ===============================
-const menuBtn = document.getElementById('menuBtn');
-const nav = document.getElementById('mainNav');
-
-menuBtn.addEventListener('click', () => {
-  nav.classList.toggle('hidden');
-});
-
-// ===============================
-// Modal (Enquiry Form) Control
-// ===============================
-function openModal() {
-  document.getElementById("enquiryModal").classList.remove("hidden");
-}
-
-function closeModal() {
-  document.getElementById("enquiryModal").classList.add("hidden");
-}
->>>>>>> 024d697dae75a8a9a8717d54452901f5408fa756
